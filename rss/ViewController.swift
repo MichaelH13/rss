6c49b2143b222d9210b55d33b45c6024b986fea3
Michael Higgins <michael@meetcircle.com>
2020-02-23 14:37:36 -0800

    Create rss Xcode project

---
+++ rss/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rss
+//
+//  Created by Circle Media Labs Inc on 2/23/20.
+//  Copyright © 2020 Objective Swift Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
